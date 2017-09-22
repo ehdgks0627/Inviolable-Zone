a807901e2c3ad94e5c0f4c6004047945519c361a
ehdgks0627 <ehdgks0627@naver.com>
2017-09-22 16:54:49 +0900

    add progressbar

--- crawler.py
+++ crawler.py
@@ -3,6 +3,7 @@ import requests
 import os
 import string
 import sys
+import progressbar
 from bs4 import BeautifulSoup
 
 def download_file(url, t):
@@ -17,15 +18,17 @@ def download_file(url, t):
     return local_filename
 
 def machine(count_start, count_end, t):
+    bar = progressbar.ProgressBar(max_value=(count_end - count_start))
+    n = 0
+    bar.update(n)
     for num in range(int(count_start/10), int(count_end/10)):
         url = "https://www.google.co.kr/search?q=example filetype:{}&start={}".format(t, num*10)
-        headers = {'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
                    'Host': 'www.google.co.kr',
                    'Connection': 'keep-alive',
                    'Cache-Control': 'max-age=0',
                    'Reffer': 'https://www.google.co.kr/'}
 
-        print("[+]" + url)
         response = requests.get(url, headers=headers)
         html = response.text
         root = BeautifulSoup(html, 'html.parser')
@@ -35,6 +38,8 @@ def machine(count_start, count_end, t):
                 link = "https://google.co.kr/url?q=" + article.find_all('a')[0]['href']
                 link = BeautifulSoup(requests.get(link, headers=headers).text, 'html.parser').find_all('div', {'class': '_jFe'})[0].find_all('a')[0]['href']
                 download_file(link, t)
+                n += 1
+                bar.update(n)
             except:
                 continue
 
