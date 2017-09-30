fe5b5597d5ac871c300cf94fb4caee7ca639332a
sprout <sprout@sprouts-MacBook-Pro.local>
2017-09-30 23:51:34 +0900

    add DiskInfo class

---
+++ client/WALLnutClient/WALLnutClient/DiskInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WALLnutClient
+{
+    public class DiskInfo
+    {
+        public string Caption { get; set; }
+        public string DeviceID { get; set; }
+        public string Model { get; set; }
+        public UInt64 Partitions { get; set; }
+        public UInt64 Size { get; set; }
+
+        public override string ToString()
+        {
+            return Caption + " (" + (Size/1024/1024/1024) + "GB)";
+        }
+    }
+}
