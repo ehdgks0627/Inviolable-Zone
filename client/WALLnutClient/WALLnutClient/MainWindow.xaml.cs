3bdc868c026741932eab0856a5226f6a5799ecb1
sprout <sprout@sprouts-MacBook-Pro.local>
2017-10-07 11:00:17 +0900

    Add WindowsAPICodePack Module

--- client/WALLnutClient/WALLnutClient/MainWindow.xaml.cs
+++ client/WALLnutClient/WALLnutClient/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Input;
-using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Management;
 using System.Runtime.InteropServices;
 using System.Text;
