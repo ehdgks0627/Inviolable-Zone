616bacdffcbfa00a4f1224044279848bc1d3ab1c
sprout <sprout@sprouts-MacBook-Pro.local>
2017-10-15 02:19:30 +0900

    add WelcomWindow

---
+++ client/WALLnutClient/WALLnutClient/WelcomeWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace WALLnutClient
+{
+    /// <summary>
+    /// Interaction logic for BlackWindow.xaml
+    /// </summary>
+    public partial class WelcomeWindow : MetroWindow
+    {
+        public WelcomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Hide();
+        }
+    }
+}
