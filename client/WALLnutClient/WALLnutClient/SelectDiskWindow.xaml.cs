3cd8dd4e17ab8b4259485cf37501338c99bb894d
ehdgks0627 <ehdgks0627@naver.com>
2017-10-15 11:22:12 +0900

    connect all window

--- client/WALLnutClient/WALLnutClient/SelectDiskWindow.xaml.cs
+++ client/WALLnutClient/WALLnutClient/SelectDiskWindow.xaml.cs
@@ -27,30 +27,41 @@ namespace WALLnutClient
             UpdateDriveList();
         }
 
-        #region [Function] 포맷 버튼 핸들러
-        private void btn_format_Click(object sender, RoutedEventArgs e)
+        #region [Function] 버튼 버튼 핸들러
+        private void btn_ok_Click(object sender, RoutedEventArgs e)
         {
+            DiskInfo info = (DiskInfo)cb_disk.Items[cb_disk.SelectedIndex];
             if (cb_disk.SelectedIndex == -1)
             {
-                MessageBox.Show("포맷할 디스크를 선택해주세요!", "에러", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("진행할 디스크를 선택해주세요!", "에러", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
-
-            if (MessageBoxResult.OK == MessageBox.Show(
+            if(info.isWALLNutDevice)
+            {
+                MainWindow window = new MainWindow();
+                window.Show();
+                this.Close();
+            }
+            else
+            {
+                if (MessageBoxResult.OK == MessageBox.Show(
                 "정말로 포맷하시겠습니까? 디스크 내 모든 데이터가 초기화됩니다!",
                 "매우 주의",
                 MessageBoxButton.OKCancel,
                 MessageBoxImage.Warning))
-            {
-                if (DiskManager.FormatDisk((DiskInfo)cb_disk.SelectedItem))
                 {
-                    MessageBox.Show("포맷 성공", "성공", MessageBoxButton.OK, MessageBoxImage.Information);
-                    UpdateDriveList();
+                    if (DiskManager.FormatDisk((DiskInfo)cb_disk.SelectedItem))
+                    {
+                        MessageBox.Show("포맷 성공", "성공", MessageBoxButton.OK, MessageBoxImage.Information);
+                        UpdateDriveList();
+                        MainWindow window = new MainWindow();
+                        window.Show();
+                        this.Close();
+                    }
                 }
             }
         }
         #endregion
-
 
         #region [Function] Drive 목록 업데이트
         public void UpdateDriveList()
@@ -65,5 +76,17 @@ namespace WALLnutClient
         }
         #endregion
 
+        private void cb_disk_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DiskInfo info = (DiskInfo)cb_disk.Items[cb_disk.SelectedIndex];
+            if(!info.isWALLNutDevice)
+            {
+                btn_ok.Content = "Format";
+            }
+            else
+            {
+                btn_ok.Content = "Go";
+            }
+        }
     }
 }
