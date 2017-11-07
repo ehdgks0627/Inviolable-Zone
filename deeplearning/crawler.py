499f557fc1ce9dcf3c99cc06e6743009c1f237f0
ehdgks0627 <ehdgks0627@naver.com>
2017-11-07 23:57:08 +0900

    update crawler

--- deeplearning/crawler.py
+++ deeplearning/crawler.py
@@ -4,6 +4,17 @@ import os
 import sys
 import progressbar
 from bs4 import BeautifulSoup
+import hashlib
+from multiprocessing import Process
+
+keywords = ["example", "source", "data", "model", "backup", "news", "lecture"]
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 def download_file(url, t):
     local_filename = url.split('/')[-1]
@@ -16,41 +27,59 @@ def download_file(url, t):
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
+
     try:
         if os.path.getsize("sample/{}/".format(t) + local_filename) < 1000:
             os.remove("sample/{}/".format(t) + local_filename)
-    except:
-        pass
-    return local_filename
-
-def machine(count_start, count_end, t):
-    bar = progressbar.ProgressBar(max_value=(count_end - count_start))
-    n = 0
-    bar.update(n)
-    for num in range(int(count_start/10), int(count_end/10)):
-        url = "https://www.google.co.kr/search?q=example filetype:{}&start={}".format(t, num*10)
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
-                   'Host': 'www.google.co.kr',
-                   'Connection': 'keep-alive',
-                   'Cache-Control': 'max-age=0',
-                   'Reffer': 'https://www.google.co.kr/'}
-
-        response = requests.get(url, headers=headers)
-        html = response.text
-        root = BeautifulSoup(html, 'html.parser')
-
-        articles = root.find_all('h3', {'class': 'r'})
-        if len(articles) == 0:
-            return
-        for article in articles:
+            return False
+        else:
             try:
-                link = "https://google.co.kr/url?q=" + article.find_all('a')[0]['href']
-                link = BeautifulSoup(requests.get(link, headers=headers).text, 'html.parser').find_all('div', {'class': '_jFe'})[0].find_all('a')[0]['href']
-                download_file(link, t)
-                n += 1
-                bar.update(n)
-            except:
-                continue
+                os.rename("sample/{}/".format(t) + local_filename, "sample/{}/".format(t) + md5("sample/{}/".format(t) + local_filename) + "." + t)
+            except FileExistsError:
+                print("exist err")
+                os.remove("sample/{}/".format(t) + local_filename)
+                return False
+    except:
+        os.remove("sample/{}/".format(t) + local_filename)
+        return False
+    return True
+
+def machine(t, download = 1000, n = 0):
+    #bar = progressbar.ProgressBar(max_value=download)
+    #bar.update(n)
+
+    print("[Thread %5d] %s start" % (os.getpid(), t))
+
+    for keyword in keywords:
+        pageIndex = 1
+        while n <= download:
+            url = "https://www.google.co.kr/search?q={}%20filetype:{}&start={}".format(keyword, t, pageIndex*10)
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+                       'Host': 'www.google.co.kr',
+                       'Connection': 'keep-alive',
+                       'Cache-Control': 'max-age=0',
+                       'Reffer': 'https://www.google.co.kr/'}
+            pageIndex += 1
+            response = requests.get(url, headers=headers)
+            print(url)
+            print(response)
+            html = response.text
+            root = BeautifulSoup(html, 'html.parser')
+
+            articles = root.find_all('h3', {'class': 'r'})
+            if len(articles) == 0:
+                break
+            for article in articles:
+                try:
+                    link = "https://google.co.kr/url?q=" + article.find_all('a')[0]['href']
+                    link = BeautifulSoup(requests.get(link, headers=headers).text, 'html.parser').find_all('div', {'class': '_jFe'})[0].find_all('a')[0]['href']
+                    print(link)
+                    if download_file(link, t):
+                        n += 1
+                        #bar.update(n)
+                except:
+                    print("err?")
+                    continue
 
 def read_ext_files(filename):
     with open(filename, "r") as f:
@@ -63,9 +92,14 @@ def main() :
         sys.exit()
     else:
         lists = read_ext_files(sys.argv[1])
+        proclist = []
         for l in lists:
             print("[+] Downloading %s..."%(l))
-            machine(0, 1000, l)
+            proc = Process(target=machine, args=(l, ))
+            proc.start()
+            proclist.append(proc)
+        for proc in proclist:
+            proc.join()
 
 if __name__ == '__main__':
     main()
