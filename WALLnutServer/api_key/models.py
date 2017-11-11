15918cca6868a7b55878a464503cce03a7464d52
ehdgks0627 <ehdgks0627@naver.com>
2017-11-11 22:38:10 +0900

    modify xor problem

--- WALLnutServer/api_key/models.py
+++ WALLnutServer/api_key/models.py
@@ -1,27 +1,6 @@
 from django.db import models
 
-
 # Create your models here.
-
-class User(models.Model):
-    access_token = models.CharField(max_length=32, primary_key=True)
-    name = models.CharField(max_length=128, blank=True)
-    api_key = models.ForeignKey(to=APIkey, on_delete=models.SET_NULL, blank=False, null=False)
-    create_time = models.DateField(blank=False, null=False)
-    last_access_time = models.DateTimeField(blank=False, null=False)
-
-    def isPaid(self):
-        return self.api_key.isPaid()
-
-    def __str__(self):
-        return "<{}, {}>".format(self.name, self.isPaid())
-
-    def __unicode__(self):
-        return "<{}, {}>".format(self.name, self.isPaid())
-
-    def __repr__(self):
-        return "<{}, {}>".format(self.name, self.isPaid())
-
 
 class APIkey(models.Model):
     serial = models.CharField(max_length=32, primary_key=True)
@@ -38,3 +17,22 @@ class APIkey(models.Model):
 
     def __repr__(self):
         return "<{}, {}>".format(self.serial, self.paid)
+
+class User(models.Model):
+    access_token = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=128, blank=True)
+    api_key = models.ForeignKey(to=APIkey, on_delete=models.SET_NULL, blank=False, null=True)
+    create_time = models.DateField(blank=False, null=False)
+    last_access_time = models.DateTimeField(blank=False, null=False)
+
+    def isPaid(self):
+        return self.api_key.isPaid()
+
+    def __str__(self):
+        return "<{}, {}>".format(self.name, self.isPaid())
+
+    def __unicode__(self):
+        return "<{}, {}>".format(self.name, self.isPaid())
+
+    def __repr__(self):
+        return "<{}, {}>".format(self.name, self.isPaid())
