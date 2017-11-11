a074e8560aafde097df89e852fba502bdff2d25e
sprout <ehdgks0627@naver.com>
2017-11-11 14:51:23 +0900

    add api_key app

--- WALLnutServer/WALLnutServer/settings.py
+++ WALLnutServer/WALLnutServer/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'classification'
+    'classification',
+    'api_key'
 ]
 
 MIDDLEWARE = [
