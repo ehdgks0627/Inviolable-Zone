9b365531088b99eb3796747a306b1eab22f79199
sprout <ehdgks0627@naver.com>
2017-11-11 21:53:49 +0900

    move folder

--- WALLnutServer/api_key/models.py
+++ WALLnutServer/api_key/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    access_token = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=128, blank=True)
+    api_key = models.ForeignKey(to=APIkey, on_delete=models.SET_NULL, blank=False, null=False)
+    create_time = models.DateField(blank=False, null=False)
+    last_access_time = models.DateTimeField(blank=False, null=False)
+
+    def isPaid(self):
+        return self.api_key.isPaid()
+
+    def __str__(self):
+        return "<{}, {}>".format(self.name, self.isPaid())
+
+    def __unicode__(self):
+        return "<{}, {}>".format(self.name, self.isPaid())
+
+    def __repr__(self):
+        return "<{}, {}>".format(self.name, self.isPaid())
+
+
+class APIkey(models.Model):
+    serial = models.CharField(max_length=32, primary_key=True)
+    paid = models.BooleanField(blank=False, null=False)
+
+    def isPaid(self):
+        return self.paid
+
+    def __str__(self):
+        return "<{}, {}>".format(self.serial, self.paid)
+
+    def __unicode__(self):
+        return "<{}, {}>".format(self.serial, self.paid)
+
+    def __repr__(self):
+        return "<{}, {}>".format(self.serial, self.paid)
