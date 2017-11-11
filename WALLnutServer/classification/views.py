0aa98b7455377ca123ca2b6254ed908d66bde3b8
sprout <ehdgks0627@naver.com>
2017-11-11 22:38:10 +0900

    modify xor problem

--- WALLnutServer/classification/views.py
+++ WALLnutServer/classification/views.py
@@ -4,10 +4,12 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.optimizers import SGD
 import numpy as np
-import keras
 
 
 def XORExample(request):
+    val1 = float(request.GET.get("val1", "0"))
+    val2 = float(request.GET.get("val2", "1"))
+    test_X = np.array([[val1, val2]], "float32")
     X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], "float32")
     y = np.array([[0], [1], [1], [0]], "float32")
 
@@ -19,4 +21,4 @@ def XORExample(request):
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='mean_squared_error', optimizer=sgd, class_mode="binary")
     model.fit(X, y, nb_epoch=1000, batch_size=1)
-    return HttpResponse(str(model.predict_proba(X)))
+    return HttpResponse(str(val1) + " ^ " + str(val2) + " = " + ("1" if model.predict_proba(test_X)[0][0] > 0.5 else "0"))
