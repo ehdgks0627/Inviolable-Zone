767f890f3686486dc48b3dd2f57aa13858a9ad27
ehdgks0627 <ehdgks0627@naver.com>
2017-11-13 10:53:49 +0900

    connected server

--- WALLnutClient/WALLnutClient/WelcomeWindow.xaml.cs
+++ WALLnutClient/WALLnutClient/WelcomeWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using System.Net.Http;
 
 namespace WALLnutClient
 {
@@ -26,8 +27,15 @@ namespace WALLnutClient
             img_logo.Source = new BitmapImage(new Uri(Properties.Resources.RESOURCES_PATH + "logo.png", UriKind.RelativeOrAbsolute));
         }
 
-        private void btn_start_Click(object sender, RoutedEventArgs e)
+        private async void btn_start_Click(object sender, RoutedEventArgs e)
         {
+            Dictionary<string, string> body = new Dictionary<string, string>
+            {
+               { "api_key", tb_apikey.Text }
+            };
+            Task<string> result = Connection.PostRequest("/v1/user/join/", body);
+            string r = await result;
+            MessageBox.Show(r);
             SelectDiskWindow window = new SelectDiskWindow();
             window.Show();
             this.Hide();
