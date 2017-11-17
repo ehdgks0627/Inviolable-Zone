787618dbc2610be5394a4505218168124636c079
TaeBbong <mok05289@korea.ac.kr>
2017-11-17 17:33:32 +0900

    Add Index Line

--- deeplearning/parse_1.py
+++ deeplearning/parse_1.py
@@ -1,5 +1,8 @@
 import os
 import csv
+import pandas as pd
+
+"""
 
 csv_dir = r'csv/'
 merged_dir = r'csv/'
@@ -22,4 +25,23 @@ for csv_name in csv_list:
 
 merged_file.close()
 
+"""
+merged_dir = r'csv/'
+merged_name = merged_dir + 'merged_png.csv'
+merged_file = open(merged_name, 'r')
 
+cleaned_dir = r'csv/'
+cleaned_name = cleaned_dir + 'cleaned_png.csv'
+cleaned_file = open(cleaned_name, 'w', newline='')
+
+
+csv_reader = csv.reader(merged_file)
+csv_writer = csv.writer(cleaned_file)
+index_line = []
+for i in range(0, 200):
+    index_line.append(i + 1)
+
+csv_writer.writerow(index_line)
+
+for row in csv_reader:
+    csv_writer.writerow(row)
